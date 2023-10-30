b72bb02710b16ef725a8e0b48b890d58798428b5
DenisPepper <thech@yandex.ru>
2023-10-30 09:48:36 +0300

    Добавит альтернативный пример экспорта/импорта

--- modules.js
+++ modules.js
@@ -1,7 +1,11 @@
 //console.log(arguments);
 //console.log(require('module').wrapper)
 
+// module.exports
 const C = require('./test-module-1.js');
-
 const calc1 = new C();
 console.log(calc1.add(1, 9));
+
+//exports
+const calc2 = require('./test-module-2.js');
+console.log(calc2.multiply(1, 9));
