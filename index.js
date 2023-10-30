a70f2f32618c4f5f83421c699ab1b284fe5a02ba
DenisPepper <thech@yandex.ru>
2023-10-30 12:27:48 +0300

    Заменит колбэки на промисы

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ const { log } = require('console');
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`${data}`);
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((response) => {
-      console.log(response.body.message);
-      fs.writeFile('dog-image.txt', response.body.message, (err) => {
-        console.log('done...');
-      });
-    })
-    .catch((err) => console.log(err.message));
-});
+const readFileAsync = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+};
+
+const writeFileAsync = (filePath, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, data, (err) => {
+      if (err) reject(err);
+      resolve('done...');
+    });
+  });
+};
+
+readFileAsync(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((response) => {
+    console.log(response.body.message);
+    return writeFileAsync('dog-image.txt', response.body.message);
+  })
+  .then((response) => console.log(response))
+  .catch((err) => console.log(err.message));
