d9e43d88a73eaaacfffacb3713ee928aab521d2a
DenisPepper <thech@yandex.ru>
2023-10-30 09:34:36 +0300

    Добавит пример экспорта/импорта

---
+++ test-module-1.js
@@ -0,0 +1,17 @@
+class Calculator {
+  constructor() {}
+
+  add(a, b) {
+    return a + b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  devide(a, b) {
+    return a / b;
+  }
+}
+
+module.exports = Calculator;
